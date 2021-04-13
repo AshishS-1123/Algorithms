170659cb51af81873731d2f09c6f5cede29bae2e
Ashish Shevale <shevaleashish@gmail.com>
2021-04-13 19:04:49 +0530

    Update coin problem

---
+++ Dynamic_Programming/coin_problem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// create a global variable to hold the list 
+
+int coin_problem_dyn( int amount )
+{
+	// if the amount is less than 0, it is not possible
+	if( amount < 0 )
+		return -1;
+	// if the amount is 0, then we dont need any coins
+	else if( amount == 0 )
+		return 0;
+	
+	// for all other amounts,
+	//
+}
+
+int main()
+{
+
+	return 0;
+}
